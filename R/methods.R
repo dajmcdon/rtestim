fce28bcc2b50bacf00ba5c21385087338071f635
Olivia Liu <jiaping.liu@stat.ubc.ca>
2022-11-29 14:37:15 -0800

    fix inconsistent arguments

--- R/methods.R
+++ R/methods.R
@@ -2,49 +2,61 @@
 #'
 #' @method summary admm_rr
 #'
-#' @param mod a fitted admm_rr model
+#' @param object a fitted admm_rr model
+#' @param ... .
 #'
 #' @return a data table of estimates and a logical value of convergence
 #'
 #' @rdname summary.admm_rr
 #'
 #' @export
-summary.admm_rr <- function(mod){
-  n = length(mod$y)
+summary.admm_rr <- function(object, ...){
+  n = length(object$y)
   res <- data.table::data.table(
-    Time = 1:n,
-    Signal = mod$y,
-    R_rate = as.vector(mod$R_rate),
-    pois_para = as.vector(mod$R_rate * mod$x)
+    Signal = object$y,
+    R_rate = as.vector(object$R_rate),
+    pois_para = as.vector(object$R_rate * object$x)
   )
-  lst = list(Results = res, Convergence = mod$convr)
+
+  lst = list(Results = res, Convergence = object$convr)
   class(lst) = "summary.admm_rr"
   return(lst)
 }
 
 #' Plot a `admm_rr' class object
 #'
-#' @method plot admm_rr
+#' @param x time
+#' @param y signal; estimated poisson mean
+#' @param ... .
 #'
-#' @param mod a fitted admm_rr model
+#' @method plot admm_rr
 #'
 #' @return a plot
 #'
 #' @rdname plot.admm_rr
 #'
 #' @export
-plot.admm_rr <- function(mod) {
-  res <- summary.admm_rr(mod)$Results
+#'
+#' @examples
+#' y <- c(rev(seq(2, 6, by = 1)), seq(2, 6, by = 1))
+#' mod = admm_solver(
+#'   y = y, x = rep(1, 10), k = 1,
+#'   init = admm_initializer(y = y, x = rep(1, 10), k = 1)
+#' )
+#' res <- summary(mod)$Results
+#' plot(res$pois_para, y)
+plot.admm_rr <- function(x, y, ...) {
+  res <- data.table::data.table(Signal = y,
+                                Time = 1:length(y),
+                                pois_para = x)
   fig <- res %>%
-    tidyr::pivot_longer(R_rate:pois_para, names_to = "Type",
-                 values_to = "Est") %>%
-    dplyr::group_by(Type) %>%
     ggplot2::ggplot(ggplot2::aes(x = Time)) +
     ggplot2::geom_point(ggplot2::aes(y = Signal)) +
-    ggplot2::geom_line(ggplot2::aes(y = Est, colour = Type, linetype = Type)) +
+    ggplot2::geom_line(ggplot2::aes(y = pois_para)) +
     ggplot2::labs(x = "Time", y = "Signal") +
-    ggplot2::scale_colour_manual(values = RColorBrewer::brewer.pal(n = 4,
+    ggplot2::scale_colour_manual(values = RColorBrewer::brewer.pal(n = 2,
                                    name = "Paired")) +
     ggplot2::theme_bw()
   fig
+  class(fig) = "plot.admm_rr"
 }
