655a301bcdd5428088c43a7127b989accf6f0d8f
Daniel J. McDonald <dajmcdon@gmail.com>
2023-02-18 11:07:37 -0800

    make header match

--- src/utils-eigen.h
+++ src/utils-eigen.h
@@ -4,5 +4,8 @@
 Eigen::SparseMatrix<double> identity(int n);
 Eigen::SparseMatrix<double> get_Dtil(int k, Rcpp::NumericVector xd);
 Eigen::SparseMatrix<double> get_D(int k, Rcpp::NumericVector xd);
+NumericVector doDv(NumericVector v, int k, NumericVector xd);
+NumericVector doDtv(NumericVector v, int k, NumericVector xd);
+NumericVector doDtDv(NumericVector v, int k, NumericVector xd);
 
 #endif
