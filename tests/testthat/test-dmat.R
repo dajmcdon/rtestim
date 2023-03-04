10a0d73080eeb45d98a3fca52da13c4e30dc7299
Daniel J. McDonald <dajmcdon@gmail.com>
2023-03-04 12:03:39 -0800

    update dmat tests (these now rely on dspline)

--- tests/testthat/test-dmat.R
+++ tests/testthat/test-dmat.R
@@ -1,35 +1,25 @@
 test_that("building the D matrix works", {
   n <- 10
   x <- 1:n
-  D1 <- buildD(10, 0)
-  D2 <- buildD(10, 1)
-  D3 <- buildD(10, 2)
-  expect_identical(D1, buildDx(n, 0, x))
-  expect_identical(D2, buildDx(n, 1, x))
-  expect_identical(D3, buildDx(n, 2, x))
-  expect_identical(D1, buildDx_tilde(n, 1, x))
-  expect_identical(D2, buildDx_tilde(n, 2, x))
-  expect_identical(D3, buildDx_tilde(n, 3, x))
+  Id <- get_D(-1, x)
+  D1 <- get_D(0, x)
+  D2 <- get_D(1, x)
+  D3 <- get_D(2, x)
+
+  # for even spacing, get_D(k-1, x) == get_Dtil(k, x)
+  Idm <- sparseMatrix(1:10, 1:10, x = 1, dims = c(10, 10))
+  expect_identical(Id, Idm)
+  expect_identical(Id, get_Dtil(0, x))
+  expect_identical(D1, get_Dtil(1, x))
+  expect_identical(D2, get_Dtil(2, x))
+
+  # note that get_Dtil() does NOT include the diagonal weighting,
+  # because we don't need it in the ADMM. So for uneven spacing,
+  # D1 %*% Dtil(2, x) != D3(x)
 
   z <- c(1:7, 9:10)
-  n <- length(z)
-  D1 <- buildD(n, 0)
-  expect_identical(D1, buildDx(n, 0, z))
-  delx <- Matrix::Diagonal(n - 1, 1 / diff(z, 1))
-  Dx2_tilde <- delx %*% D1
-  Dx2 <- D1[1:(n - 2), 1:(n - 1)] %*% Dx2_tilde
-  expect_identical(Dx2_tilde, buildDx_tilde(n, 1, z))
-  expect_identical(Dx2, buildDx(n, 1, z))
-  delx <- Matrix::Diagonal(n - 2, 2 / diff(z, 2))
-  Dx3_tilde <- delx %*% Dx2
-  Dx3 <- D1[1:(n - 3), 1:(n - 2)] %*% Dx3_tilde
-  expect_identical(Dx3_tilde, buildDx_tilde(n, 2, z))
-  expect_identical(Dx3, buildDx(n, 2, z))
+  expect_identical(get_D(2, z), dspline::d_mat(3, z, TRUE)) # note 2 vs 3
+  expect_identical(get_Dtil(2, z), dspline::d_mat(2, z, FALSE))
 
 })
 
-test_that("building D works with empty x", {
-  x = double(0) # no
-  expect_identical(buildD(10, 2), buildDx(10, 2, x))
-  expect_identical(buildDx_tilde(10, 2, x), buildDx_tilde(10, 2, 1:10))
-})
