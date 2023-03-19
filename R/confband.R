63436b31d8ed5e365b2216d1524ff81dcc9244d2
Daniel J. McDonald <dajmcdon@gmail.com>
2023-03-19 10:59:31 -0700

    add documentation

--- R/confband.R
+++ R/confband.R
@@ -1,11 +1,51 @@
 
+
+#' Add confidence bands to estimated Rt curves
+#'
+#' Create an approximate confidence band for the Rt estimate. Note that the
+#' variance computation is approximate.
+#'
+#' @param object a `poisson_rt` or `cv_poisson_rt` object.
+#' @param lambda the selected lambda. May be a scalar value, or in the case of
+#'  `cv_poisson_rt` objects, `"lambda.min"` or `"lambda.max"`.
+#' @param level the desired confidence level(s). These will be sorted if
+#'   necessary.
+#' @param ... additional arguments for methods. Unused.
+#'
+#' @return A list with containing the estimated `Rt` at the chosen `lambda`,
+#'  and `lower` and `upper` confidence limits corresponding to `level`
+#' @export
+#'
+#' @examples
+#' y <- c(1, rpois(100, dnorm(1:100, 50, 15)*500 + 1))
+#' out <- estimate_rt(y, nsol = 10)
+#' confband(out, out$lambda[2])
+#' confband(out, out$lambda[2], level = c(0.95, 0.8, 0.5))
 confband <- function(object, lambda, level = 0.95, ...) {
+  UseMethod("confband")
+}
+
+#' @export
+confband.cv_poisson_rt <- function(object,
+                                   lambda = c("lambda.min", "lambda.1se"),
+                                   level = 0.95, ...) {
+  rlang::check_dots_empty()
+  arg_is_probabilities(level)
+  if (is.character(lambda)) lambda <- object[[match.arg(lambda)]]
+  else arg_is_numeric(lambda)
+  confband(object$full_fit, lambda = lambda, level = level)
+}
+
+#' @export
+confband.poisson_rt <- function(object, lambda, level = 0.95, ...) {
+  rlang::check_dots_empty()
   arg_is_numeric(lambda)
   arg_is_scalar(lambda)
   arg_is_probabilities(level)
+  level <- sort(level, decreasing = TRUE)
 
   nbd <- function(n, ord) {
-    if (n < ord + 1) return(Diagonal(n, x = 0))
+    if (n < ord + 1) return(Matrix::Diagonal(n, x = 0))
     buildD(n, ord)
   }
 
@@ -13,23 +53,25 @@ confband <- function(object, lambda, level = 0.95, ...) {
   n <- length(y)
   rhat <- fitted(object, lambda)
   yhat <- predict(object, lambda)
-  k <- object$degree
   knots <- find_knots(object, lambda)
-  Ds <- Matrix::bdiag(lapply(knots$lens, nbd, ord = k))
+  Ds <- Matrix::bdiag(lapply(knots$lens, nbd, ord = object$degree))
   # The procedure for this approximation:
-  # 0. Pretend we knew the knots (and lambda is fixed / known) --> Ds
+  # 0. Pretend we knew the knots (and lambda is fixed + known) --> Ds
   # 1. pretend we had used ||Ds r||_2^2 with the same lambda
   # 2. Apply multivariate delta method
   #   a. Var(y) = (I * exp(theta) + lambda (Ds'Ds))^(-1)
   #   b. g(theta) = exp(theta) / w
   #   c. g'(theta) = exp(theta) / w = r
   #   d. Need Var(y) * g'(theta)^2 -->
-  covs <- diag(solve(Matrix::Diagonal(n, yhat) + lambda * crossprod(Ds))) *
-    rhat^2
+  covs <- diag(Matrix::solve(
+    Matrix::Diagonal(n, yhat) + lambda * Matrix::crossprod(Ds)
+  )) * rhat^2
+  lower <- drop(outer(sqrt(covs), stats::qt((1 - level) / 2, n - knots$dof)))
+  upper <- drop(outer(sqrt(covs), stats::qt(level / 2, n - knots$dof)))
   return(list(
     Rt = rhat,
-    lower = pmax(rhat + qt(1 - level / 2, n - knots$dof) * sqrt(covs), 0),
-    upper = pmax(rhat + qt(level / 2, n - knots$dof) * sqrt(covs), 0)
+    lower = pmax(rhat + lower, 0),
+    upper = pmax(rhat + upper, 0)
   ))
 }
 
