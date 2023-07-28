b9b4923931ed70dabe5add335af2a67be270352f
Daniel J. McDonald <dajmcdon@gmail.com>
2023-07-28 15:05:06 -0700

    all checks pass

--- tests/testthat/test-cv.R
+++ tests/testthat/test-cv.R
@@ -1,8 +1,6 @@
 
 
 test_that("fold index helper function works", {
-  n <- 10
-  fold <- 4
-  expected_idx <- c(0, 1, 2, 3, 4, 1, 2, 3, 4, 0)
-  expect_equal(expected_idx, fold_calculator(n, fold))
+  skip("This function was removed.")
+
 })
