ed7407c659e96c1c25ec5a9aff78d011ab226114
Olivia Liu <jiaping.liu@stat.ubc.ca>
2023-10-05 22:07:09 -0700

    add a test on CV

--- tests/testthat/test-cv.R
+++ tests/testthat/test-cv.R
@@ -1,6 +1,9 @@
+test_that("cv passes correct parameters to inner solvers (when lamdba is not
+          specified)", {
+            y <- c(1, rpois(100, dnorm(1:100, 50, 15) * 500 + 1))
+            cv <- cv_estimate_rt(y, korder = 3, nfold = 3, nsol = 30)
+            mod <- estimate_rt(y, korder = 3, nsol = 30)
 
+            expect_identical(cv$lambda, mod$lambda)
+          })
 
-test_that("fold index helper function works", {
-  skip("This function was removed.")
-
-})
