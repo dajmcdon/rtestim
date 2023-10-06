685166842ab0062f20270d56625e889108b77a7c
Olivia Liu <jiaping.liu@stat.ubc.ca>
2023-10-05 22:13:36 -0700

    add another test on CV

--- tests/testthat/test-cv.R
+++ tests/testthat/test-cv.R
@@ -7,3 +7,13 @@ test_that("cv passes correct parameters to inner solvers (when lamdba is not
             expect_identical(cv$lambda, mod$lambda)
           })
 
+test_that("test CV returns an error message when max iteration is too low for
+          the size of candidate set & vice versa", {
+  set.seed(1001)
+  y <- c(1, rpois(100, dnorm(1:100, 50, 15) * 500 + 1))
+  expect_error(
+    cv_estimate_rt(y, korder = 3, nfold = 3, nsol = 400))
+  expect_no_error(
+    cv_estimate_rt(y, korder = 3, nfold = 3, nsol = 300)
+    )
+})
