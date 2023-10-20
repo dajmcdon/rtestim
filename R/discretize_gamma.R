10771640978e07f345a65f16e8cdce73e70d522b
Daniel J. McDonald <dajmcdon@gmail.com>
2023-10-20 16:52:43 -0700

    better error handling

--- R/discretize_gamma.R
+++ R/discretize_gamma.R
@@ -17,7 +17,9 @@
 #' @examples
 #' discretize_gamma(1:30, shape = 1, scale = 1)
 discretize_gamma <- function(x, shape = 2.5, scale = 2.5, rate = 1 / scale) {
-  stopifnot(shape > 0, scale > 0, all(x >= 0))
+  arg_is_numeric_scalar(shape, scale, rate)
+  arg_is_nonnegative(x)
+  if (is.unsorted(x)) cli::cli_abort("`x` must be sorted in increasing order.")
   pgm <- stats::pgamma(x, shape = shape, scale = scale)
   pgm <- c(0, pgm)
   pgm <- diff(pgm)
